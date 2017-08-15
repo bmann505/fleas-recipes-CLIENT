6fa9fcc7fb2642eaedda7a586d10254c7ff1583d
Brian Mann <bmann505@gamil.com>
2017-08-15 16:17:57 -0600

    signup route

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
-  console.log('hello world');
-})
+  $('#myModal').on('shown.bs.modal', function() {
+    $('#myInput').focus()
+  })
+  const $signUpSbmtBtn = $('#signUpSubmit');
+
+  $signUpSbmtBtn.click(signUp);
+});
+
+const baseURL = 'http://localhost:8080/'
+const signUpURL = `${baseURL}signup`
+
+function signUp(event) {
+  event.preventDefault();
+  let name = $('#signUpName').val();
+  let email = $('#signUpEmail').val();
+  let password = $('#signUpPassword').val();
+  let data = {
+    name,
+    email,
+    password
+  }
+  console.log(data)
+  $.post(signUpURL, data)
+    .then(response => {
+      if (response.error) {
+        alert(response.error)
+      } else {
+        location.href = '/'
+      }
+    })
+}
