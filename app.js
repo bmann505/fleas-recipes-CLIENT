cc705ca15b9fbe835a241e74516cf0284447ca58
Brian Mann <bmann505@gamil.com>
2017-08-15 21:12:27 -0600

    login

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ $(document).ready(function() {
     $('#myInput').focus()
   })
   const $signUpSbmtBtn = $('#signUpSubmit');
-
+  const $loginSbmtBtn = $('#loginSubmit');
   $signUpSbmtBtn.click(signUp);
+  $loginSbmtBtn.click(login);
 });
 
 const baseURL = 'http://localhost:8080/'
 const signUpURL = `${baseURL}signup`
+const loginURL = `${baseURL}login`
 
 function signUp(event) {
   event.preventDefault();
@@ -25,6 +27,25 @@ function signUp(event) {
     .then(response => {
       if (response.error) {
         alert(response.error)
+      } else {
+        location.href = '/'
+      }
+    })
+}
+
+function login(event) {
+  event.preventDefault();
+  let email = $('#loginEmail').val();
+  let password = $('#loginPassword').val();
+  let data = {
+    email,
+    password
+  }
+  $.post(loginURL, data)
+    .then(response => {
+      if (response.error) {
+        alert(response.error);
+        location.href = '/'
       } else {
         location.href = '/'
       }
